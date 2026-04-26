632688de5beb341ef287cb4f0542c6c95326e7e4
Kyle Wong <kylewxng06@gmail.com>
2026-04-25 18:50:02 -0700

    dsajbds

--- overlay_standalone/preload.cjs
+++ overlay_standalone/preload.cjs
@@ -8,9 +8,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   getConfig:            ()       => ipcRenderer.invoke('get-config'),
   createSession:        (data)   => ipcRenderer.invoke('create-session', data),
   endSession:           (data)   => ipcRenderer.invoke('end-session', data),
-<<<<<<< HEAD
-  pauseSession:         ()       => ipcRenderer.invoke('pause-session'),
-=======
   updateFocusState:     (state)  => ipcRenderer.invoke('update-focus-state', state),
->>>>>>> 5c21c71b6cd8ea341a561471b39da79bc8d967b4
 });
